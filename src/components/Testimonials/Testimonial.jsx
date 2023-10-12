f2e30993f84cbf0ea7fb731ad23ee0c2e4755136
SMB_developer <sbate989@gmail.com>
2023-10-13 00:16:25 +0530

    first commit

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jpg";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      review:
+        "SMB Web Developer completed the entire project within the new deadline and did an excellent job. The project management team was always responsive and available when needed. They embraced the company's goals and helped to push the product to be even better",
+    },
+    {
+      img: profilePic2,
+      review:
+        "Was looking for expert team to develop Zillow Clone for my real estate business . After going through the demo i was very happy to select SMB Web Developer. They delivered the complex solution in 1 month time and i am very happy with there services .",
+    },
+    {
+      img: profilePic3,
+      review:
+        "SMB Web Developer always exceeds expectations by contributing a valuable and educated viewpoint to engagements. Their organization was a standout feature. Customers may count on a team of technical specialists to solve problems and meet deadlines.",
+    },
+    {
+      img: profilePic4,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+    },
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+      <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
+      <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
